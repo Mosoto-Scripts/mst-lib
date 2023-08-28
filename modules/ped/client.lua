fb1c15fd14f9a41962b3fb776d63e981728c2a77
JustStoyanov <stoyanow2707@gmail.com>
2023-08-28 16:17:19 -0400

    GetHashKey --> joaat - fixed field model to either string or number

--- modules/ped/client.lua
+++ modules/ped/client.lua
@@ -1,12 +1,12 @@
 ---@class PedData
----@field model string
+---@field model string | number
 ---@field coords vector3
 ---@field rotation number
 
 ---@param data PedData
 ---@return table
 mlib.ped = function(data)
-    local model, c = GetHashKey(data.model), data.coords;
+    local model, c = joaat(data.model), data.coords;
     local x, y, z, r = c.x, c.y, c.z, data.rotation;
     -- Ped Creation --
     lib.requestModel(model);
