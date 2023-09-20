ec8e0b47ee52fa790f470576c706d07b566f2994
JustStoyanov <stoyanow2707@gmail.com>
2023-09-20 05:58:15 -0400

    Changed purpose

--- core/import.lua
+++ core/import.lua
@@ -1,12 +1,12 @@
 local debug_getinfo = debug.getinfo;
 mlib = setmetatable({
-    name = 'mst-lib',
+    name = 'king-lib',
     context = IsDuplicityVersion() and 'server' or 'client',
 }, {
     __newindex = function(self, name, fn)
         rawset(self, name, fn);
 
-        if debug_getinfo(2, 'S').short_src:find('@mst-lib/resource') then
+        if debug_getinfo(2, 'S').short_src:find('@king-lib/resource') then
             exports(name, fn);
         end
     end
